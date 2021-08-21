b580c852ddeff7528d841d42f9ff232426c5750a
cerm88 <cerm1202@hotmail.com>
2021-08-21 17:48:30 -0400

    Vista index y botones de acción

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@
 */
 
 // Ruta raiz, nos mostrará todos los posts
+
+// use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/','PageController@posts');
 
 // Ruta para ir al blog y el post seleccionado
@@ -20,3 +24,8 @@ Route::get('/blog{post}','PageController@post')->name('post');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Ruta de tipo recursos, las rutas priavdas deberían estar en la carpeta Backend/
+Route::resource('posts', 'Backend\PostController')
+    ->middleware('auth')
+    ->except('show'); // No queremos que desde la parte privada nos muestre el método show
