4de964ddd337219f5daafac0e3c2d324970d9490
cerm88 <cerm1202@hotmail.com>
2021-08-21 23:06:56 -0400

    Implementación del guardado

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -36,9 +36,21 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
-        //
+        // Salvar, gardamos el post
+        $post = Post::create([
+            'user_id' => auth()->user()->id
+        ] + $request->all());
+
+        // Imagen, si hay una imagen entonces actualiza el post con la imagen
+        if ($request->file('file')) {
+            $post->image = $request->file('file')->store('posts', 'public');
+            $post->save();
+        }
+
+        // Retornar
+        return back()->with('status', 'Creado con éxito');
     }
 
     /**
--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,10 @@ class Post extends Model
 {
     use Sluggable;
 
+    protected $fillable = [
+        'title', 'body', 'iframe', 'image', 'user_id',
+    ];
+
     /**
      * Return the sluggable configuration array for this model.
      *
