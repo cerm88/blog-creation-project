cf34b1bacaedae2e2e86e1b6ffd59f70d240febd
cerm88 <cerm1202@hotmail.com>
2021-08-22 01:46:33 -0400

    Finalizando reto de mostrar contenido embebido e imagen en el post individual

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,17 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card mb-4">
+                {{-- Colocando archivo multimedia --}}
+                @if ($post->image)
+                    <img src="{{ $post->get_image }}" class="card-img-top">
+                @endif
                 <div class="card-body">
-                    {{-- Colocando archivo multimedia --}}
-                    @if ($post->image)
-                        <img src="{{ $post->get_image }}" class="card-img-top">
-                    @elseif ($post->iframe)
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    @if ($post->iframe)
                         {{-- Para imprimir código html --}}
                         <div class="embed-responsive embed-responsive-16by9">
                             {!! $post->iframe !!}
                         </div>
                     @endif
-                    <h5 class="card-title">{{ $post->title }}</h5>
                     <p class="card-text">
                         {{ $post->body }}
                     </p>
